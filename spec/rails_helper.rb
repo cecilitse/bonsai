aa0321a04acbf55f3c1d9ae1e172dd33a0ca1140
Cecile Veneziani <contact@cecilitse.org>
2018-12-21 10:17:13 +0100

    Testing - Add system test on garden plants
    - Add test on plants listing
    - Add test on plant details, creation, edition, deletion
    - Add test an marking / unmarking a plant as favorite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,7 @@ Capybara.javascript_driver = :firefox_headless
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :system
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
