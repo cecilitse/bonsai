6f6b88f64fd53dc6abdd839b4ff49fe245067c23
Cecile Veneziani <contact@cecilitse.org>
2022-05-31 20:52:33 +0200

    Update schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -49,4 +49,6 @@ ActiveRecord::Schema.define(version: 2018_12_09_233946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "care_moments", "plants"
+  add_foreign_key "plants", "users"
 end
