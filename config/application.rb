4fb30badd5d0f252c1f694b9a6704bbbbf7db8fe
Cecile Veneziani <contact@cecilitse.org>
2022-08-07 16:26:20 +0200

    Upgrade rails to 7.0

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -27,10 +27,6 @@ module Bonsai
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_233946) do
+ActiveRecord::Schema[6.1].define(version: 2018_12_09_233946) do
 
   create_table "care_moments", force: :cascade do |t|
     t.integer "plant_id"
