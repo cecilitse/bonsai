332a4eb304b23db7a70406f44d88ae607f9017be
Cecile Veneziani <contact@cecilitse.org>
2022-08-07 16:39:31 +0200

    Set application rails defaults to 7.0

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,7 @@ module Bonsai
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-
+    config.load_defaults 7.0
 
     # Don't generate system test files.
     config.generators.system_tests = nil
